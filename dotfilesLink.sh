a18bf9f48c04d4a0e5ca3ea5246fd189696bdfdb
r.tomiyama <r.tomiyama.gc@future.co.jp>
2020-03-25 12:21:34 +0900

    update

---
+++ dotfilesLink.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+ln -sf ~/dotfiles/vim/vimrc ~/.vimrc
+ln -sf ~/dotfiles/bash/bash_profile ~/.bash_profile
+ln -sf ~/dotfiles/bash/bashrc ~/.bashrc
+ln -sf ~/dotfiles/zsh/zsh_profile ~/.zsh_profile
+ln -sf ~/dotfiles/zsh/zshrc ~/.zshrc
+ln -sf ~/dotfiles/config/tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/config/dircolors ~/.dircolors
